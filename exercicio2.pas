50ab3275767a57051239eb6506f2ea673572117b
Julio9009 <56002423+Julio9009@users.noreply.github.com>
2026-03-06 14:20:31 -0300

    Add files via upload

---
+++ exercicio2.pas
@@ -0,0 +1,17 @@
+
+program Exercicio1;
+
+var 
+    a , b , result : integer;
+
+begin
+
+    Write('Por favor o primeiro numero: ');
+    readLn(a);
+    write(' Por favor digite o segundo ');
+    readLn(b);
+    result := a + b;
+    
+    writeln (' O resultado da soma eh ' , result);
+    
+end.
---
+++ exercicio4.pas
@@ -0,0 +1,16 @@
+
+program Exercicio4;
+
+var 
+    contador : integer;
+
+begin
+
+    contador := 1;
+    while contador <= 30 do 
+        begin 
+            writeLn (contador); 
+            contador := contador + 1;
+        end
+    
+end.
