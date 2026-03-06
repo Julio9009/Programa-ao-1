e65dca6fb197517d78159ec6bdf363a379dba872
Julio9009 <56002423+Julio9009@users.noreply.github.com>
2026-03-06 15:10:06 -0300

    Add files via upload

---
+++ Aula 3 Programação/exercicio1.pas
@@ -0,0 +1,13 @@
+
+program Exercicio1;
+
+var 
+    nome : string;
+
+begin
+
+    Write('Por favor, digite seu nome: ');
+    readLn(nome);
+    writeln ('Ola ', nome , ' Seja bem vindo!');
+    
+end.
